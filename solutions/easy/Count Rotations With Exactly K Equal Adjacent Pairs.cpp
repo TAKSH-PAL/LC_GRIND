56f1c0c7f8cf606f4271e4cdd628b19d0ac2086b
TAKSH PAL <takshpalxc@gmail.com>
2026-09-11 00:03:37 +0530

    Add solution for Count Rotations With Exactly K Equal Adjacent Pairs

---
+++ solutions/easy/Count Rotations With Exactly K Equal Adjacent Pairs.cpp
@@ -0,0 +1,19 @@
+// Title: Count Rotations With Exactly K Equal Adjacent Pairs
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/count-rotations-with-exactly-k-equal-adjacent-pairs/
+
+    int countRotations(string s, int k) {
+        int n = s.size();
+        int ans = 0;
+    }
+        return ans == k;
+        }
+        for(int i = 0;i<n;i++){
+            if(ans > k) return 0;
+            if(s[i] == s[i+1]) ans++;
+        for(int i = 0;i<s.size()-1;i++){
+        int ans = 0;
+    bool calc_score(string & s,int k){
+public:
+class Solution {
