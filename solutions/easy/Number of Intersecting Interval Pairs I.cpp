4750b6d30e09592f19753146ef6d1f7cf8796517
TAKSH PAL <takshpalxc@gmail.com>
2026-09-20 20:11:15 +0530

    Add solution for Number of Intersecting Interval Pairs I

---
+++ solutions/easy/Number of Intersecting Interval Pairs I.cpp
@@ -0,0 +1,19 @@
+// Title: Number of Intersecting Interval Pairs I
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/number-of-intersecting-interval-pairs-i/
+
+        int n = intervals.size();
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+            }
+        }
+        return ans;
+    intervals) {
+                if(intervals[i][0]>intervals[j][1] || intervals
+    int countIntersectingIntervals(vector<vector<int>>& 
+    }
+                [i][1]<intervals[j][0]) continue;
+                else ans++;
+};
